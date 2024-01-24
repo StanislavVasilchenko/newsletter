39b3d35356f81adaeead524312a42279dd4effdc
stanislav <stanislav.vasilchenko@yandex.ru>
2024-01-24 11:25:33 +0300

    register, authentication

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import UserRegistrationView, UserProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserChangeForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
-# Create your views here.
+from users.forms import UserRegistrationForm, UserProfileForm
+from users.models import User
+
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
